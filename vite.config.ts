22196f399691d49cfa5678422d15b63f20ff9b84
klamts <ktsquoclam@gmail.com>
2025-08-13 14:35:43 +0700

    Add project modified package-lock.json and vite.cònig.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/audio-flashcard-image-generator/', // tên repo của bạn
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
